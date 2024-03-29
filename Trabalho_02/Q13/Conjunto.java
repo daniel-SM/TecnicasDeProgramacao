ad5e298f550323292252732f3055239588867ead
Daniel Santos <danieldsm.on@gmail.com>
2024-03-29 15:46:28 -0300

    Resolution of question 13 of exercise 02.

---
+++ Trabalho_02/Q13/Conjunto.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+
+public class Conjunto<T> {
+    private ArrayList<T> elementos;
+
+    Conjunto() {
+        this.elementos = new ArrayList<T>();
+    }
+
+    void insert (T valor) {
+        if (!this.contem(valor)) {
+            this.elementos.add(valor);
+        }
+    }
+
+    boolean contem (T valor) {
+        return this.elementos.contains(valor);
+    }
+
+    boolean subconjunto (Conjunto<T> outro) {
+        for (T elemento : this.elementos) {
+            if (!outro.contem(elemento)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Conjunto<T> uniao (Conjunto<T> outro) {
+        Conjunto<T> novo = new Conjunto<T>();
+
+        for (T elemento : this.elementos) {
+            novo.insert(elemento);
+        }
+
+        for (T elemento : outro.elementos) {
+            novo.insert(elemento);
+        }
+
+        return novo;
+    }
+
+    Conjunto<T> intersecao (Conjunto<T> outro) {
+        Conjunto<T> novo = new Conjunto<T>();
+
+        for (T elemento : outro.elementos) {
+            if (outro.contem(elemento)) {
+                novo.insert(elemento);
+            }
+        }
+
+        return novo;
+    }
+
+    Conjunto<T> diferenca (Conjunto<T> outro) {
+        Conjunto<T> novo = new Conjunto<T>();
+
+        for (T elemento : this.elementos) {
+            if(!outro.contem(elemento)) {
+                novo.insert(elemento);
+            }
+        }
+
+        return novo;
+    }
+
+    boolean igual (Conjunto<T> outro) {
+        if (this.elementos.size() == outro.elementos.size()) {
+            for (int i = 0; i < this.elementos.size(); i++) {
+                if (this.elementos.get(i) != outro.elementos.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String valores = "[";
+
+        for (T elemento : this.elementos) {
+            valores = valores + " " + elemento;
+        }
+        valores = valores + " ]";
+
+        return valores;
+    }
+};
---
+++ Trabalho_02/Q13/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+        Conjunto<Integer> naturais = new Conjunto<>();
+        naturais.insert(0);
+        naturais.insert(1);
+        naturais.insert(2);
+        naturais.insert(3);
+        naturais.insert(4);
+        naturais.insert(5);
+        naturais.insert(6);
+        naturais.insert(7);
+        naturais.insert(8);
+        naturais.insert(9);
+
+        Conjunto<Integer> pares = new Conjunto<>();
+        pares.insert(0);
+        pares.insert(2);
+        pares.insert(4);
+        pares.insert(6);
+        pares.insert(8);
+        
+        Conjunto<Integer> impares = new Conjunto<>();
+        impares.insert(1);
+        impares.insert(3);
+        impares.insert(5);
+        impares.insert(7);
+        impares.insert(9);
+
+        Conjunto<Integer> primos = new Conjunto<>();
+        primos.insert(2);
+        primos.insert(3);
+        primos.insert(5);
+        primos.insert(7);
+        primos.insert(11);
+        primos.insert(13);
+        primos.insert(17);
+        primos.insert(19);
+        primos.insert(23);
+        primos.insert(29);
+
+        System.out.println("Se o conjunto 4 é subconjunto de si mesmo: " 
+            + primos.subconjunto(primos));
+        System.out.println("A pertinência dos conjuntos 2 no conjunto 1: "
+            + pares.subconjunto(naturais));
+        System.out.println("A pertinência dos conjuntos 3 no conjunto 1: "
+            + impares.subconjunto(naturais));
+        System.out.println("A pertinência dos conjuntos 4 no conjunto 1: "
+            + primos.subconjunto(naturais));
+        System.out.println("Se a união de 2 e 3 é igual ao conjunto 1: "
+            + naturais.igual(pares.uniao(impares)));
+        System.out.println("Se a intersecção dos conjuntos 1 e 2 é vazia: "
+            + naturais.intersecao(pares));
+        System.out.println("Qual diferença entre os conjuntos 1 e 2: "
+            + naturais.diferenca(pares));
+    }
+}
