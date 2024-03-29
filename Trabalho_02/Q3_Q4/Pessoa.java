928582de9a0120c760bc3698be9b82f61d6ad3fc
Daniel Santos <danieldsm.on@gmail.com>
2024-03-29 15:44:38 -0300

    Resolution of questions 3 and 4 of exercise 02.

---
+++ Trabalho_02/Q3_Q4/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    String nome;
+    int idade;
+
+    void setNome (String pnome) {
+        nome = pnome;
+    }
+
+    String getNome () {
+        return nome;
+    }
+}
